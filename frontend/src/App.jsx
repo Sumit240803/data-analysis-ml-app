30455a70d08f18e08ace08bcb58fbe6ebd80a4d5
Sumit240803 <goyalsumit651@gmail.com>
2025-01-26 23:03:10 +0530

    Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,53 +1,16 @@
 
-import { useState } from 'react'
 import './App.css'
-import SimpleBarChart from './components/Barcharts/BarChart'
-import axios from "axios"
-import StackedBarChart from './components/Barcharts/Stackbarchart'
-import SimpleLineChart from './components/Linecharts/SimplelineChar'
-import BasicPie from './components/Piecharts/Piecharts'
+
 
 function App() {
-  const[file,setFile] = useState(null);
-  const [columns , setColumns ] = useState([]);
-  const handleFileChange =(event)=>{
-    setFile(event.target.files[0]);
-  }  
-  const handleSubmit = async(event)=>{
-    event.preventDefault();
-
-    const formData = new FormData();
-    formData.append('file', file);
-    try {
-      
-      const response = await axios.post('http://127.0.0.1:5000/clean/upload-csv', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-      console.log('Response from server:', response.data);
-      setColumns(response.data.columns)
-    } catch (error) {
-      console.error('Error uploading file:', error);
-    }
-  }
-
   return (
     <>
-    <form onSubmit={handleSubmit}>
+    <div className='landing-container'>
 
-    <input type="file" name="file" onChange={handleFileChange} id="file" />
-    <button type='submit'>Submit</button>
-    </form>
-    {columns.length>0 && columns.map((column)=>(
-      <div key={column}>
-        {column}
-      </div>
-    ))}
-      <SimpleBarChart/>
-     
-      <BasicPie/>
-      <SimpleLineChart/>
+    <div className='img-container'>
+      <img className='img' src="/Landing-Images/1.png" alt="" />
+    </div>
+    </div>
     </>
   )
 }
