c9745076568802b728f89db49cb4be792a598e64
Sumit240803 <goyalsumit651@gmail.com>
2025-01-28 15:51:47 +0530

    Styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,22 @@ function App() {
       
       <div className='heading'>
       <h2>Meaningful Insights of your Data in seconds</h2>
+      <div className='info-row'>
+
+      <div className='info-row-1'>
+
+      <span>Predict</span>
+      <span>Preprocess</span>
+      <span>Train</span>
       </div>
+      <div className='info-row-1'>
+        <span>Visualize</span>
+        <span>Analyze</span>
+      </div>
+      <div className='info-row-1'><span>Model</span></div>
+      </div>
+      </div>
+      
     </div>
     </>
   )
